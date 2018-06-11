a9b56a928957b1ffd85928a11573dfb079e96689
kySuga <kyle@masuga.com>
2018-06-10 22:08:45 -0600

    Add image alt tags

--- js/main.js
+++ js/main.js
@@ -140,6 +140,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = `${restaurant.name} of ${restaurant.neighborhood}`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
